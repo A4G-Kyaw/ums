2bcb1d216035b03c9839e9494f1832959ed0e2cf
“A4G-Kyaw” <agkyaw.akk1921@gmail.com>
2023-02-23 13:50:11 +0630

    second time

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var indexRouter = require('./app/routes/index');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
- })
+// view path and engine 
+app.set('views',path.join(__dirname,'app/views'));
+app.set('view engine', 'ejs');
+
+// set public folder 
+app.use(express.static(__dirname + '/public'));
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', function (req, res) {
+//     res.render('auth/login');
+//  })
+app.use('/', indexRouter);
  
  var server = app.listen(8089, function () {
     var host = server.address().address
